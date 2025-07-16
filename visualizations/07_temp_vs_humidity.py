bfac3fef68112d4b3c497b9a24e6f789c80e923e
lorin07 <fawazkakahoun@gmail.com>
2025-07-16 12:55:49 +0200

    pdf, rendu final

--- visualizations/07_temp_vs_humidity.py
+++ visualizations/07_temp_vs_humidity.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 df = pd.read_csv("data/raw/weather_observations.csv")
 sns.jointplot(x="temperature", y="humidity", data=df, kind="hex", cmap="viridis")
 plt.suptitle("Corrélation température / humidité", y=1.02)
